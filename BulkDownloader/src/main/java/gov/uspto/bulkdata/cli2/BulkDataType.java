433cf5835f876b6507b11b934afc67086d11c25e
Guiqiang Zhang <skyhit@gmail.com>
2017-12-12 15:48:22 +0800

    update structure and scripts

--- BulkDownloader/src/main/java/gov/uspto/bulkdata/cli2/BulkDataType.java
+++ BulkDownloader/src/main/java/gov/uspto/bulkdata/cli2/BulkDataType.java
@@ -3,20 +3,21 @@ package gov.uspto.bulkdata.cli2;
 import java.net.MalformedURLException;
 
 public enum BulkDataType {
-		
-	GAZETTE("data2/patent/officialgazzette/", "zip"),
 
-	GRANT_MULTI_PAGE_IMAGES("data3/patent/grant/multipagetiff/", "zip"),
+	//03/15/2017 NOTE:  The disk space on BDSS is being restructured. URLs/links that contain data2 and data3 will be changing to data.
+	GAZETTE("data/patent/officialgazzette/", "zip"),
+
+	GRANT_MULTI_PAGE_IMAGES("data/patent/grant/multipagetiff/", "zip"),
 	GRANT_SINGLE_PAGE_IMAGES("data/patent/grant/yellowbook/", "tar"),
 	GRANT_REDBOOK_WITH_IMAGES("grant/redbook/", "tar"),
-	GRANT_REDBOOK_TEXT("data2/patent/grant/redbook/fulltext/", "zip"),
-	GRANT_REDBOOK_BIBLIO("data2/patent/grant/redbook/bibliographic/", "zip"),
+	GRANT_REDBOOK_TEXT("data/patent/grant/redbook/fulltext/", "zip"),
+	GRANT_REDBOOK_BIBLIO("data/patent/grant/redbook/bibliographic/", "zip"),
 
-	APPLICATION_MULTI_PAGE_IMAGES("data3/patent/application/multipagetiff/", "zip"),
+	APPLICATION_MULTI_PAGE_IMAGES("data/patent/application/multipagetiff/", "zip"),
 	APPLICATION_SINGLE_PAGE_IMAGES("data/patent/application/yellowbook/", "tar"),
-	APPLICATION_REDBOOK_WITH_IMAGES("data2/patent/application/redbook/", "tar"),
-	APPLICATION_REDBOOK_TEXT("data2/patent/application/redbook/fulltext/", "zip"),
-	APPLICATION_REDBOOK_BIBLIO("data2/patent/grant/redbook/bibliographic/", "zip");
+	APPLICATION_REDBOOK_WITH_IMAGES("data/patent/application/redbook/", "tar"),
+	APPLICATION_REDBOOK_TEXT("data/patent/application/redbook/fulltext/", "zip"),
+	APPLICATION_REDBOOK_BIBLIO("data/patent/grant/redbook/bibliographic/", "zip");
 
 	private static String BASEURL = "https://bulkdata.uspto.gov/";
 	private String restPath;
--- scripts/download.sh
+++ scripts/download.sh
@@ -3,21 +3,23 @@
 # Download USPTO Weekly Bulk Patent Dumps
 #
 
-LIMIT=1
+DATE=$1
+
 
 # ProjectPath is one directory up from script bin directory.
 PROJECTPATH=$( cd $(dirname $0)/.. ; pwd -P )
 
-CLASSPATH="${PROJECTPATH}/lib/*:${PROJECTPATH}/lib/dependency-jars/*"
+CLASSPATH="${PROJECTPATH}/BulkDownloader/target/*:${PROJECTPATH}/BulkDownloader/target/dependency-jars/*"
 
 JAVA="java -cp ${CLASSPATH} -Dlog4j.configuration=file:${PROJECTPATH}/conf/log4j.properties"
 
 #
-# type: [application, grant]    requited;  patent document type
-# years     required; comma for individual years; dash for year range
+# type: [application, grant, gazette]    requited;  patent document type
+# date     required; 20140101-20161231
 # limit     download limit
 # skip      skip over limit
+# async     Async Downloads
 # filename  specific bulk file name to download
 # outdir    directory to download to.
 #
-${JAVA} gov.uspto.bulkdata.cli2.BulkData --source google --type application --limit=${LIMIT} --outdir="${PROJECTPATH}/download"
+${JAVA} gov.uspto.bulkdata.cli2.BulkData --type application --date $DATE --outdir="${PROJECTPATH}/download"
